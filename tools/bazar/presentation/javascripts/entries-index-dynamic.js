b4c9fdde2f0b7c5084bd751b875bd6227f43aec8
Sebastian Castro <sebastian.castro@protonmail.com>
2025-01-18 19:14:43 +0100

    refactor(bazarlist): extract image code to mixin

--- tools/bazar/presentation/javascripts/entries-index-dynamic.js
+++ tools/bazar/presentation/javascripts/entries-index-dynamic.js
@@ -3,16 +3,18 @@ import EntryField from './components/EntryField.js'
 import PopupEntryField from './components/PopupEntryField.js'
 import SpinnerLoader from './components/SpinnerLoader.js'
 import ModalEntry from './components/ModalEntry.js'
-import BazarSearch from './components/BazarSearch.js'
 import FilterNode from './components/FilterNode.js'
 import { initEntryMaps } from './fields/map-field-map-entry.js'
-import { recursivelyCalculateRelations } from './utils.js'
+import { recursivelyCalculateRelations, deepGet } from './utils.js'
+import ImageMixin from './entries-index-dynamic/image-mixin.js'
+import BazarSearch from './entries-index-dynamic/search-mixin.js'
 
 Vue.component('FilterNode', FilterNode)
 
 const load = (domElement) => {
   new Vue({
     el: domElement,
+    mixins: [BazarSearch, ImageMixin],
     components: {
       Panel,
       ModalEntry,
@@ -20,7 +22,6 @@ const load = (domElement) => {
       EntryField,
       PopupEntryField
     },
-    mixins: [BazarSearch],
     data: {
       mounted: false, // when vue get initialized
       ready: false, // when ajax data have been retrieved
@@ -37,9 +38,7 @@ const load = (domElement) => {
 
       currentPage: 0,
       pagination: 10,
-      tokenForImages: null,
-      imagesToProcess: [],
-      processingImage: false,
+
       search: '',
       currentSort: { field: '', asc: null, label: '' },
 
@@ -134,7 +133,6 @@ const load = (domElement) => {
         this.filterEntries()
       },
       filterEntries() {
-        // Handles filters
         let result = this.searchedEntries
         Object.entries(this.computedFilters).forEach(([propName, filter]) => {
           result = result.filter((entry) => {
@@ -343,124 +341,6 @@ const load = (domElement) => {
           values = values.filter((val) => this.computedFilters[field].includes(val))
         }
         return mapping[values[0]]
-      },
-      urlImageResizedOnError(entry, fieldName, width, height, mode, token) {
-        const node = event.target
-        $(node).removeAttr('onerror')
-        if (entry[fieldName]) {
-          const fileName = entry[fieldName]
-          if (!this.isExternalUrl(entry)) {
-            // currently not supporting api for external images (anti-csrf token not generated)
-            if (this.tokenForImages === null) {
-              this.tokenForImages = token
-            }
-            this.imagesToProcess.push({
-              fileName,
-              width,
-              height,
-              mode,
-              node
-            })
-            this.processNextImage()
-          } else {
-            const baseUrl = entry.url
-              .slice(0, -entry.id_fiche.length)
-              .replace(/\?$/, '')
-              .replace(/\/$/, '')
-            const previousUrl = $(node).prop('src')
-            const newUrl = `${baseUrl}/files/${fileName}`
-            if (newUrl != previousUrl) {
-              $(`img[src="${previousUrl}"]`).each(function() {
-                $(this).prop('src', newUrl)
-              })
-            }
-          }
-        }
-      },
-      urlImage(entry, fieldName, width, height, mode) {
-        if (!entry[fieldName]) {
-          return null
-        }
-        let baseUrl = this.isExternalUrl(entry)
-          ? entry.url.slice(0, -entry.id_fiche.length)
-          : wiki.baseUrl
-        baseUrl = baseUrl.replace(/\?$/, '').replace(/\/$/, '')
-        const fileName = entry[fieldName]
-        const field = this.fieldInfo(fieldName)
-
-        if (fileName.endsWith('.svg')) return `${baseUrl}/files/${fileName}`
-
-        let regExp = new RegExp(
-          `^(${entry.id_fiche}_${field.propertyname}_.*)_(\\d{14})_(\\d{14})\\.([^.]+)$`
-        )
-
-        if (regExp.test(fileName)) {
-          return `${baseUrl}/cache/${fileName.replace(regExp, `$1_${mode == 'fit' ? 'vignette' : 'cropped'}_${width}_${height}_$2_$3.$4`)}`
-        }
-        regExp = new RegExp(
-          `^(${entry.id_fiche}_${field.propertyname}_.*)\\.([^.]+)$`
-        )
-        if (regExp.test(fileName)) {
-          return `${baseUrl}/cache/${fileName.replace(regExp, `$1_${mode == 'fit' ? 'vignette' : 'cropped'}_${width}_${height}.$2`)}`
-        }
-        // maybe from other entry
-        regExp = new RegExp(
-          `^([A-Za-z0-9-_]+_${field.propertyname}_.*)_(\\d{14})_(\\d{14})\\.([^.]+)$`
-        )
-        if (regExp.test(fileName)) {
-          return `${baseUrl}/cache/${fileName.replace(regExp, `$1_${mode == 'fit' ? 'vignette' : 'cropped'}_${width}_${height}_$2_$3.$4`)}`
-        }
-        // last possible format
-        regExp = new RegExp('^(.*)\\.([^.]+)$')
-        if (regExp.test(fileName)) {
-          return `${baseUrl}/cache/${fileName.replace(regExp, `$1_${mode == 'fit' ? 'vignette' : 'cropped'}_${width}_${height}.$2`)}`
-        }
-        return `${baseUrl}/files/${fileName}`
-      },
-      processNextImage() {
-        if (!this.processingImage && this.imagesToProcess.length > 0) {
-          this.processingImage = true
-          const newImageParams = this.imagesToProcess[0]
-          this.imagesToProcess = this.imagesToProcess.slice(1)
-          const bazarListDynamicRoot = this
-          $.ajax({
-            url: wiki.url(
-              `?api/images/${newImageParams.fileName}/cache/${newImageParams.width}/${newImageParams.height}/${newImageParams.mode}`
-            ),
-            method: 'post',
-            data: { csrftoken: this.tokenForImages },
-            cache: false,
-            success(data) {
-              const previousUrl = $(newImageParams.node).prop('src')
-              const srcFileName = wiki.baseUrl.replace(/(\?)?$/, '') + data.cachefilename
-              $(`img[src="${previousUrl}"]`).each(function() {
-                $(this).prop('src', srcFileName)
-                const next = $(this).next('div.area.visual-area[style]')
-                if (next.length > 0) {
-                  const backgoundImage = $(next).css('background-image')
-                  if (
-                    backgoundImage != undefined
-                    && typeof backgoundImage == 'string'
-                    && backgoundImage.length > 0
-                  ) {
-                    $(next).css('background-image', '') // reset to force update
-                    $(next).css('background-image', `url("${srcFileName}")`)
-                  }
-                }
-              })
-            },
-            complete(e) {
-              if (
-                e.responseJSON != undefined
-                && e.responseJSON.newToken != undefined
-              ) {
-                bazarListDynamicRoot.tokenForImages = e.responseJSON.newToken
-              }
-              bazarListDynamicRoot.processingImage = false
-              bazarListDynamicRoot.processNextImage()
-            }
-          })
-        }
       }
     },
     mounted() {
