717fcb251454c68044180772b3cb3b52a40b8181
Marsnet Rv <rv@marslibre.net>
2025-01-20 04:24:44 +0000

    Translated using Weblate (Romanian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: YesWiki/YesWiki extension Contact js
    Translate-URL: https://hosted.weblate.org/projects/yeswiki/yeswiki-extension-contact-js/ro/

--- tools/contact/lang/contactjs_ro.inc.php
+++ tools/contact/lang/contactjs_ro.inc.php
@@ -1,6 +1,6 @@
 <?php
 return [
     // libs/contact.js
-    'CONTACT_REQUIRED_FIELD' => '',
-    'CONTACT_EMAIL_NOT_VALID' => '',
+    'CONTACT_REQUIRED_FIELD' => 'Acest câmp este obligatoriu.',
+    'CONTACT_EMAIL_NOT_VALID' => 'E-mailul introdus nu este valid.',
 ];
