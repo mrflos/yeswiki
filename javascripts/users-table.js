f355313000bd140559f0b5be48d41b19988ef8d8
Sebastian Castro <sebastian.castro@protonmail.com>
2025-01-22 17:00:38 +0100

    feat(userstable) use icon instead of text

--- javascripts/users-table.js
+++ javascripts/users-table.js
@@ -31,7 +31,6 @@ const usersTableService = {
             '',
             userEmail,
             signuptime,
-            '',
             ''
           ]).draw()
           if (userLink !== '') {
